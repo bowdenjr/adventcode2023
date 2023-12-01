de2942099b6727f66d2c54793572468f6e26e7c8
Jonathan Bowden <jonathan.bowden@hyperexponential.com>
2023-12-01 18:02:08 +0000

    day 1 refactor

--- day_01/part1.py
+++ day_01/part1.py
@@ -1,16 +1,9 @@
 
 def get_number_from_first_and_last_digit(line):
 
-    line_digits_only = []
-
-    for char in line:
-        if char.isdigit():
-            line_digits_only.append(char)
-
-    if len(line_digits_only) > 1:
-        digit = int(line_digits_only[0] + line_digits_only[-1])
-    else:
-        digit = int(line_digits_only[0] + line_digits_only[0])
+    line_digits_only = [char for char in line if char.isdigit()]
+    
+    digit = int(line_digits_only[0] + line_digits_only[-1])
 
     return digit
 
@@ -18,12 +11,7 @@ def part1():
 
     f = open("day_01\input_day_01.txt")
 
-    numbers_to_add = []
-
-    for line in f.readlines():
-        
-        digit = get_number_from_first_and_last_digit(line)
-        numbers_to_add.append(digit)
+    numbers_to_add = [get_number_from_first_and_last_digit(line) for line in f.readlines()]
 
     print(sum(numbers_to_add)) # Actual answer = 55029
 
